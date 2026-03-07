0d4b54dfb613dd6ca2400e85aac971b81528b23e
Harsh <gajjarharsh570@gmail.com>
2026-03-07 21:38:52 +0530

    custom command for development

--- e_commerce/utils/management/commands/runall.py
+++ e_commerce/utils/management/commands/runall.py
@@ -1,5 +1,6 @@
 from django.core.management import BaseCommand, call_command
 from django.contrib.auth import get_user_model
+from django.conf import settings
 
 User = get_user_model()
 
@@ -8,13 +9,14 @@ class Command(BaseCommand):
     help = "run all initial commands if any changes happen with models."
 
     def handle(self, *args, **options):
-        try:
-          call_command("makemigrations")
-          call_command("migrate")
+      if getattr(settings, 'DEBUG'):
+          try:
+            call_command("makemigrations")
+            call_command("migrate")
 
-          if not User.objects.filter(is_superuser=True).exists():
-              call_command("createsuperuser")
+            if not User.objects.filter(is_superuser=True).exists():
+                call_command("createsuperuser")
 
-          call_command("runserver")
-        except Exception as e:
-          print(str(e))
+            call_command("runserver")
+          except Exception as e:
+            print(str(e))
