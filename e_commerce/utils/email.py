8f59a549e405b593e66e65d62c549c14de060f25
Harsh <gajjarharsh570@gmail.com>
2026-03-12 11:24:08 +0530

    fix threading error

--- e_commerce/utils/email.py
+++ e_commerce/utils/email.py
@@ -2,17 +2,18 @@ from django.core.mail import send_mail
 from django.conf import settings
 import threading
 
-def purchase_success_email(recipient_list=None):
+def purchase_success_email(recipient_email=None):
   subject = "JRAT's E-Commerce"
   message = f"You have successfully purchased. Check Your Order Below Link.\nhttp://127.0.0.1:8000/account/profile/"
   from_email = getattr(settings, 'EMAIL_HOST_USER')
-  recipient_list = [recipient_list]
+  recipient_list = [recipient_email]
 
-  email = send_mail(subject=subject, message=message, from_email=from_email, recipient_list=recipient_list)
-
-  threading.Thread(
-    target=email, args=(subject, message, from_email, recipient_list)
-  ).start()
+  try:
+    threading.Thread(
+      target=send_mail, args=(subject, message, from_email, recipient_list)
+    ).start()
+  except Exception as e:
+    pass
 
 def send_reset_password_email(email,  reset_password_email_link):
   subject = "JRAT's E-Commerce reset password link"
@@ -20,9 +21,10 @@ def send_reset_password_email(email,  reset_password_email_link):
   from_email = getattr(settings, 'EMAIL_HOST_USER')
   recipient_list = [email]
 
-  email = send_mail(subject=subject, message=message, from_email=from_email, recipient_list=recipient_list)
+  try:
 
-  threading.Thread(
-    target=email, args=(subject, message, from_email, recipient_list)
-  ).start()
-
+    threading.Thread(
+      target=send_mail, args=(subject, message, from_email, recipient_list)
+    ).start()
+  except Exception as e:
+   pass
