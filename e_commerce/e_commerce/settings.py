d35b160808af3cd74463ed795b128bf170c09dba
Harsh <gajjarharsh570@gmail.com>
2026-03-12 19:04:48 +0530

    cloudinary setup for media files in production

--- e_commerce/e_commerce/settings.py
+++ e_commerce/e_commerce/settings.py
@@ -204,7 +204,7 @@ MESSAGE_TAGS = {
     messages.ERROR:   'error',
 }
 
-#whitenoise cache setup
+#whitenoise cache setup for static files
 STORAGES = {
     "default": {
         "BACKEND": "django.core.files.storage.FileSystemStorage",
@@ -212,4 +212,15 @@ STORAGES = {
     "staticfiles": {
         "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
     },
+}
+
+# cloudinary for media files
+INSTALLED_APPS += ["cloudinary", "cloudinary_storage"]
+
+DEFAULT_FILE_STORAGE = "cloudinary_storage.storage.MediaCloudinaryStorage"
+
+CLOUDINARY_STORAGE = {
+    "CLOUD_NAME": config("CLOUDINARY_CLOUD_NAME"),
+    "API_KEY": config("CLOUDINARY_API_KEY"),
+    "API_SECRET": config("CLOUDINARY_API_SECRET"),
 }
